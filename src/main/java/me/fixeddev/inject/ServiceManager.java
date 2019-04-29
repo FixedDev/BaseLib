66cf7d38eaeac095aa23368a270e8052c8a7337b
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-04-29 00:09:06 -0500

    Updated the ServiceManager to allow PluginServices
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- src/main/java/me/fixeddev/inject/ServiceManager.java
+++ src/main/java/me/fixeddev/inject/ServiceManager.java
@@ -2,8 +2,12 @@ package me.fixeddev.inject;
 
 import com.google.inject.ImplementedBy;
 import com.google.inject.Inject;
+import me.fixeddev.service.PluginService;
 import me.fixeddev.service.Service;
+import me.ggamer55.bcm.parametric.ParametricCommandHandler;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginLogger;
+import org.bukkit.plugin.PluginManager;
 
 import java.util.Set;
 import java.util.logging.Level;
@@ -20,11 +24,25 @@ public interface ServiceManager {
         private Set<Service> services;
         @Inject
         private PluginLogger logger;
+        @Inject
+        private PluginManager pluginManager;
+        @Inject
+        private Plugin plugin;
+        @Inject
+        private ParametricCommandHandler commandHandler;
 
         @Override
         public void start() {
             for (Service service : services) {
+
                 try {
+                    if (service instanceof PluginService) {
+                        PluginService pluginService = (PluginService) service;
+
+                        pluginService.registerCommands(commandHandler);
+                        pluginService.registerListeners(plugin,pluginManager);
+                    }
+
                     service.start();
                 } catch (Exception ex) {
                     logger.log(Level.SEVERE, "An error ocurred while a service was being enabled", ex);
